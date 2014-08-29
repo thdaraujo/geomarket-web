59556d814ad9f61ca503f0f6a2dd938c10a15534
Luiz <lhsperez@gmail.com>
2014-08-29 00:54:17 +0000

    Gente do 87 pode entrar no site

--- app/models/estabelecimento.rb
+++ app/models/estabelecimento.rb
@@ -7,7 +7,7 @@ class Estabelecimento < ActiveRecord::Base
   EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\Z/i
   validates :nome, :presence => true, :uniqueness => true, :length => { :in => 3..50 }
   validates :logradouro, :presence => true, :uniqueness => false, :length => { :in => 3..50 }
-  validates :numero, :presence => true, :uniqueness => false, :length => { :in => 3..15 }
+  validates :numero, :presence => true, :uniqueness => false
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   
   validates :password, :presence => true, :length => { :in => 6..20 }, :confirmation => true, :on => :create
