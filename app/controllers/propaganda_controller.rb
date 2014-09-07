59f19b86f5e4b3cb0cd34f5ab265aa66652c419c
Luiz <lhsperez@gmail.com>
2014-09-07 23:02:17 +0000

    Menu de usuário na navbar

--- app/controllers/propaganda_controller.rb
+++ app/controllers/propaganda_controller.rb
@@ -1,6 +1,6 @@
 class PropagandaController < ApplicationController  
   
-  before_filter :authenticate_user, :only => [:new, :create]
+  before_filter :authenticate_user
   
   def new
     @propaganda = Propaganda.new
